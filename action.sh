86629a025de4172f29124936e494596734c3a201
kyuzato <82192424+kyuzato@users.noreply.github.com>
2022-05-01 09:31:38 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,10 @@
+sudo apt install git curl python3-pip ffmpeg -y
+pip3 install -U pip
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
+sudo apt-get install -y nodejs
+npm i -g npm
 git clone https://github.com/vexido/video-stream /root/vexido
 cp CREDS/.env /root/vexido/.env
 cd /root/vexido
-docker build . -t claes
-docker run --name musik claes
+pip3 install -U -r requirements.txt
+python3 main.py
