3d358a7305b41f1e36a3c98e96d34a8f6054391b
kyuzato <82192424+kyuzato@users.noreply.github.com>
2022-05-06 14:15:18 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,10 +1,9 @@
-sudo apt install git curl python3-pip ffmpeg -y
-pip3 install -U pip
-curl -sL https://deb.nodesource.com/setup_16.x | bash -
-sudo apt-get install -y nodejs
-npm i -g npm
-git clone https://github.com/vexido/video-stream keqing
-cp CREDS/.env keqing/.env
-cd keqing
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install python3-pip ffmpeg -y
+sudo pip3 install -U pip
+curl -fssL https://deb.nodesource.com/setup_17.x | sudo -E bash - && sudo apt-get install nodejs -y && npm i -g npm
+git clone https://github.com/vexido/YukkiMusicBot Keqing
+cp CREDS/.env Keqing/.env
+cd Keqing
 pip3 install -U -r requirements.txt
-python3 main.py
+bash start
