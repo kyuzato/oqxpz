8a20a292720c2bd56dc30a89b855f0e0411dc7f1
kyuzato <82192424+kyuzato@users.noreply.github.com>
2022-05-31 01:26:13 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,3 +1,6 @@
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
+sudo apt-get install -y nodejs
+npm i -g npm
 git clone https://github.com/vexido/remix /root/keqing
 cp CREDS/.env /root/keqing/.env
 cd /root/keqing
