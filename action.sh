dd2f7b85e3ad5d3f6acf01cf4056c22af243bb27
kyuzato <82192424+kyuzato@users.noreply.github.com>
2022-05-31 01:20:38 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/vexido/remix /root/keqing
 cp CREDS/.env /root/keqing/.env
 cd /root/keqing
-npm install && npm run build
-npm start
+docker build . -t keqing
+docker run keqing
