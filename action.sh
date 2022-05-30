eb2bc6c702aff28089a4efb1a9ed4b5d1fc0abe0
kyuzato <82192424+kyuzato@users.noreply.github.com>
2022-05-31 01:11:19 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,10 +1,5 @@
-sudo apt install git curl python3-pip ffmpeg -y
-pip3 install -U pip
-curl -sL https://deb.nodesource.com/setup_16.x | bash -
-sudo apt-get install -y nodejs
-npm i -g npm
-git clone https://github.com/erickevannicholas/video-stream /root/keqing
+git clone https://github.com/vexido/remix /root/keqing
 cp CREDS/.env /root/keqing/.env
 cd /root/keqing
-pip3 install -U -r requirements.txt
-python3 main.py
+npm install && npm run build
+npm start
