aa3e41a9a2ec712f51ae58b46e4ad90107446954
kyuzato <82192424+kyuzato@users.noreply.github.com>
2022-06-01 04:32:21 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,9 +1,5 @@
-sudo apt install git ffmpeg -y
-curl -sL https://deb.nodesource.com/setup_16.x | bash -
-sudo apt-get install -y nodejs
-npm i -g npm
-git clone https://github.com/vexido/remix /root/keqing
+git clone https://github.com/erickevannicholas/video-stream /root/keqing
 cp CREDS/.env /root/keqing/.env
 cd /root/keqing
-npm install && npm run build
-npm start
+docker build . -t keqingmusic
+docker run keqingmusic
