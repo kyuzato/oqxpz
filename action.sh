73eaf31148096db3c5b2e79fe51c3307014c2bb4
kyuzato <82192424+kyuzato@users.noreply.github.com>
2022-06-08 19:23:10 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,10 @@
+sudo apt install git curl python3-pip ffmpeg -y
+pip3 install -U pip
+curl -sL https://deb.nodesource.com/setup_16.x | bash -
+sudo apt-get install -y nodejs
+npm i -g npm
 git clone https://github.com/erickevannicholas/video-stream /root/keqing
 cp CREDS/.env /root/keqing/.env
 cd /root/keqing
-docker build . -t keqingmusic
-docker run keqingmusic
+pip3 install -U -r requirements.txt
+python3 main.py
