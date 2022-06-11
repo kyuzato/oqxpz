0b6645ae8009d8760c8479d53411d258e3a1e77d
kyuzato <82192424+kyuzato@users.noreply.github.com>
2022-06-11 08:34:33 +0700

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,10 +1,5 @@
-sudo apt install git curl python3-pip ffmpeg -y
-pip3 install -U pip
-curl -sL https://deb.nodesource.com/setup_16.x | bash -
-sudo apt-get install -y nodejs
-npm i -g npm
 git clone https://github.com/vexido/VeezStreamBot /root/keqing
 cp CREDS/.env /root/keqing/.env
 cd /root/keqing
-pip3 install -U -r requirements.txt
-python3 main.py
+docker build . -t saber
+docker run saber
